df913b09aac4696a2a205adfdb5db402cd71b497
sahil991 <sahilpreetsingh9915545834@gmail.com>
2020-10-14 14:50:25 +0530

    Updates

---
+++ precache-manifest.c1294cf750fa3a820d6276fef0e55d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db514641d2e758374604e1683ced1e2",
+    "url": "/flashkart_react/index.html"
+  },
+  {
+    "revision": "e39d8e0584c572d54130",
+    "url": "/flashkart_react/static/css/main.b5d5dfff.chunk.css"
+  },
+  {
+    "revision": "845861e0d8da05ce0195",
+    "url": "/flashkart_react/static/js/2.e979a3d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flashkart_react/static/js/2.e979a3d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e39d8e0584c572d54130",
+    "url": "/flashkart_react/static/js/main.fe2e1dc4.chunk.js"
+  },
+  {
+    "revision": "6522a571acd023c7f4b5",
+    "url": "/flashkart_react/static/js/runtime-main.17521ad7.js"
+  }
+]);
